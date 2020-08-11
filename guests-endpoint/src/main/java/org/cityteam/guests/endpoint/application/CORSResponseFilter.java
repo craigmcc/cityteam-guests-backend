ccaef86a3b5e370d62a3e4604ef847436afed036
craigmcc <craigmcc@gmail.com>
2020-08-11 10:42:50 -0700

    Some more work on CORSResponseFilter so that Axios calls in the NodeJS front end will not complain.

---
+++ guests-endpoint/src/main/java/org/cityteam/guests/endpoint/application/CORSResponseFilter.java
@@ -0,0 +1,46 @@
+/*
+ * Copyright 2020 CityTeam, craigmcc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cityteam.guests.endpoint.application;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+/**
+ * <p>JAX-RS response filter to add a CORS header that allows
+ * access from anywhere.</p>
+ */
+@Provider
+public class CORSResponseFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter
+            (ContainerRequestContext requestContext,
+             ContainerResponseContext responseContext)
+            throws IOException {
+
+        MultivaluedMap<String, Object> headers =
+                responseContext.getHeaders();
+        // TODO - might need to narrow some of these down
+        headers.add("Access-Control-Allow-Headers", "*");
+        headers.add("Access-Control-Allow-Origin", "*");
+
+    }
+
+}
